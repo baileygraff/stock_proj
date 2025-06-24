0c0aba7ef9528fc034a9d19422dc9e2d57754331
baileygraff <95446813+baileygraff@users.noreply.github.com>
2025-06-24 13:55:17 -0500

    adjusted extract script to tag extracted stock data with a 'stock' value and the stock symbol

--- scripts/ext_stock_data.py
+++ scripts/ext_stock_data.py
@@ -19,15 +19,41 @@ def extract_init_kagdata(dataset_dict: dict, filepath="raw_data"):
     try:
         print("extracting datasets from list")  #consider making this a more specific/descriptive message
         kaggle.api.authenticate()   #kaggle api token needs to be setup
-        data_destination = Path(filepath)     #double check the logic on this
-        for key in dataset_dict:
-            target_dataset = str(key)
-            target_file = str(dataset_dict[key])   #select the value to the key, which is the dataset name
-            kaggle.api.dataset_download_file(target_dataset, target_file,data_destination, force=True)
-            print(f"{target_dataset} dataset extracted to {data_destination} directory")
+        data_destination = Path(filepath)
+        data_destination.mkdir(parents=True, exist_ok=True) 
+        #does this address some of my need for creating directories if not already made? 
+
+        for dataset_id, (_, target_file) in dataset_dict.items():
+            kaggle.api.dataset_download_file(dataset_id, target_file, path=data_destination, force=True)
+            print(f"{dataset_id} dataset extracted to {data_destination} directory")
     except Exception as e:
         print("Kaggle dataset extraction failed: ", e)
         #is there something to return that makes this fail in the most safe way?
+
+def add_stock_column(dataset_dictionary: dict, csv_path: str = "raw_data"):
+    #function to append "stock" column with stock symbol value
+    #what if this is not needed and is run by mistake?
+
+    data_path = Path(csv_path)
+
+    try:
+        for dataset_key in dataset_dictionary:
+            stock, filename = dataset_dictionary[dataset_key]
+            csv_filepath = data_path / filename
+            
+            if not csv_filepath.exists():
+                print(f"Skipping missing file: {csv_filepath}")
+                continue
+
+            df = pd.read_csv(csv_filepath)
+
+            if "stock" not in df.columns:
+                df["stock"] = stock
+                df.to_csv(csv_filepath, index=False)
+
+    except Exception as e:
+        print("failed adding stock column to csv: ", e)
+
 
 """
 not working yet -- to be worked out later if possible
